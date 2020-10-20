432e38ee0d2dae476874c831522a9774142b75d9
Vitor <bigellivitor@gmail.com>
2020-10-20 10:39:26 -0300

    Add form steps as placeholders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import RegisterForm from './pages/Register';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <h1> biz2 </h1>    
+      <h1> biz2 </h1> 
+      <Container>
+      <RegisterForm /> 
+      </Container>  
     </div>
   );
 }
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react'
+import GeneralForm from './components/General'
 
 const RegisterForm  = () => {
 
     const [ step, updateStep ] = useState(0) 
+    const [ formData, updateFormData ] = useState({})
 
+    console.log(step)
 
     switch (step) {
         case 0: 
-            return <h3> Step 1 - Dados Gerais </h3>
+            return ( 
+                <> 
+                    <h3> Step 1 - Dados Gerais </h3>
+                    <GeneralForm onNext={() => updateStep(step + 1)} />
+                </>
+            )
         case 1: 
-            return <h3> Step 2 - Dados Pessoais </h3>
+            return ( 
+                <>  
+                    <h3> Step 2 - Dados Pessoais </h3>
+                    <GeneralForm onNext={() => updateStep(step + 1)} />
+                </>
+            )
         case 2: 
-            return <h3> Step 3 - Dados Profissionais</h3>
-        case 2: 
-            return <h3> Step 3 - Dados Bancários </h3>
+        return ( 
+                <>
+                    <h3> Step 3 - Dados Profissionais</h3>
+                    <GeneralForm onNext={() => updateStep(step + 1)} />
+                </>
+            )
+        case 3: 
+            return ( 
+                <> 
+                    <h3> Step 3 - Dados Bancários </h3>
+                    <GeneralForm onNext={() => updateStep(step + 1)} />
+                </>
+            )
     }
 
 
