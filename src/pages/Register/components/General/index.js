9fc96404473956ee87e3576b08521e346e627a37
Vitor <bigellivitor@gmail.com>
2020-10-20 11:41:59 -0300

    Add account type field to general form

--- src/pages/Register/components/General/index.js
+++ src/pages/Register/components/General/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react' 
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Col, Row } from 'react-bootstrap'
 
 const GeneralForm = ({ onNext }) => {
     
@@ -21,23 +21,41 @@ const GeneralForm = ({ onNext }) => {
                 <Form.Control type="text" placeholder="" value={fullName} onChange={(e) => updateFullName(e.target.value) } />
             </Form.Group>
             
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => updateEmail(e.target.value) } />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+            <Row>
+
+                <Form.Group controlId="formBasicEmail" as={Col}>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => updateEmail(e.target.value) } />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+
+                <Form.Group controlId="accountType" as={Col}>
+                    <Form.Label>Tipo de conta</Form.Label>
+                    <Form.Control as="select" placeholder="Selecione" value={accountType}  onChange={(e) => updateAccountType(e.target.value) }>
+                        <option> Pessoa Física </option>
+                        <option> Pessoa Jurídica </option>          
+                    </Form.Control>
+                </Form.Group>
+
+
+            </Row>
         
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Senha</Form.Label>
-                <Form.Control type="password" placeholder="Password" value={password}  onChange={(e) => updatePassword(e.target.value) } /> 
-            </Form.Group>
-        
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Confirmar senha</Form.Label>
-                <Form.Control type="password" placeholder="Password" value={confirmPassword}  onChange={(e) => updateConfirmPassword(e.target.value) } />
-            </Form.Group>
+            <Row>
+
+                <Form.Group controlId="formBasicPassword" as={Col}>
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control type="password" placeholder="Password" value={password}  onChange={(e) => updatePassword(e.target.value) } /> 
+                </Form.Group>
+            
+                <Form.Group controlId="formBasicPassword" as={Col}>
+                    <Form.Label>Confirmar senha</Form.Label>
+                    <Form.Control type="password" placeholder="Password" value={confirmPassword}  onChange={(e) => updateConfirmPassword(e.target.value) } />
+                </Form.Group>
+
+            </Row>
             
             <Button variant="primary" type="submit" onClick={ (e) => onSubmit(e) }>
                 Next 
--- src/pages/Register/components/Personal/index.js
+++ src/pages/Register/components/Personal/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react' 
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Row, Col } from 'react-bootstrap'
 
 const PersonalForm = ({ onNext }) => {
     
@@ -17,52 +17,64 @@ const PersonalForm = ({ onNext }) => {
 
     return (
         <Form>
-            <Form.Group controlId="cpf">
-                <Form.Label>CPF</Form.Label>
-                <Form.Control type="text" placeholder="" value={cpf} onChange={(e) => updateCpf(e.target.value) } />
-            </Form.Group>
+            <Row>
+
+                <Form.Group controlId="cpf" as={Col}>
+                    <Form.Label>CPF</Form.Label>
+                    <Form.Control type="text" placeholder="" value={cpf} onChange={(e) => updateCpf(e.target.value) } />
+                </Form.Group>
+                
+                <Form.Group controlId="rg" as={Col}>
+                    <Form.Label>RG</Form.Label>
+                    <Form.Control type="text" placeholder="" value={rg} onChange={(e) => updateRg(e.target.value) } />
+                </Form.Group>
+
+            </Row>
+            <Row>
+
+                <Form.Group controlId="birthday" as={Col}>
+                    <Form.Label>Data de Nascimento</Form.Label>
+                    <Form.Control type="date" placeholder="01/01/1900" value={birthday}  onChange={(e) => updateBirthday(e.target.value) } /> 
+                </Form.Group>
             
-            <Form.Group controlId="rg">
-                <Form.Label>RG</Form.Label>
-                <Form.Control type="text" placeholder="" value={rg} onChange={(e) => updateRg(e.target.value) } />
-            </Form.Group>
+                <Form.Group controlId="status" as={Col}>
+                    <Form.Label>Estado Civil</Form.Label>
+                    <Form.Control as="select" placeholder="Selecione" value={status}  onChange={(e) => updateStatus(e.target.value) }>
+                        <option> Solteiro(a) </option>
+                        <option> Casado(a) </option>
+                        <option> Separado(a) </option>
+                        <option> Viúvo(a) </option>
+                        <option> Divorciado(a) </option>
+                        
+                    </Form.Control>
+                </Form.Group>
+
+            </Row>
+
+            <Row>
+
         
-            <Form.Group controlId="birthday">
-                <Form.Label>Data de Nascimento</Form.Label>
-                <Form.Control type="date" placeholder="01/01/1900" value={birthday}  onChange={(e) => updateBirthday(e.target.value) } /> 
-            </Form.Group>
-        
-            <Form.Group controlId="status">
-                <Form.Label>Estado Civil</Form.Label>
-                <Form.Control as="select" placeholder="Selecione" value={status}  onChange={(e) => updateStatus(e.target.value) }>
-                    <option> Solteiro(a) </option>
-                    <option> Casado(a) </option>
-                    <option> Separado(a) </option>
-                    <option> Viúvo(a) </option>
-                    <option> Divorciado(a) </option>
-                    
-                </Form.Control>
-            </Form.Group>
-        
-            <Form.Group controlId="phone">
-                <Form.Label>Telefone</Form.Label>
-                <Form.Control type="phone" placeholder="(11) 99999 9999" value={phone}  onChange={(e) => updatePhone(e.target.value) } />
-            </Form.Group>
-        
-            <Form.Group controlId="gender">
-                <Form.Label>Gênero</Form.Label>
-                <Form.Control as="select" placeholder="Selecione" value={gender}  onChange={(e) => updateStatus(e.target.value) }>
-                    <option> Masculino </option>
-                    <option> Feminino </option>
-                    <option> Transgênero </option>
-                    <option> Transexual </option>
-                    <option> Cisgênero </option>
-                    <option> Não sei responder </option>
-                    <option> Prefiro não responder </option>
-                    <option> Outros </option>                    
-                </Form.Control>
-            </Form.Group>
+                <Form.Group controlId="phone" as={Col}>
+                    <Form.Label>Telefone</Form.Label>
+                    <Form.Control type="phone" placeholder="(11) 99999 9999" value={phone}  onChange={(e) => updatePhone(e.target.value) } />
+                </Form.Group>
             
+                <Form.Group controlId="gender" as={Col}>
+                    <Form.Label>Gênero</Form.Label>
+                    <Form.Control as="select" placeholder="Selecione" value={gender}  onChange={(e) => updateGender(e.target.value) }>
+                        <option> Masculino </option>
+                        <option> Feminino </option>
+                        <option> Transgênero </option>
+                        <option> Transexual </option>
+                        <option> Cisgênero </option>
+                        <option> Não sei responder </option>
+                        <option> Prefiro não responder </option>
+                        <option> Outros </option>                    
+                    </Form.Control>
+                </Form.Group>
+            
+
+            </Row>
             <Button variant="primary" type="submit" onClick={ (e) => onSubmit(e) }>
                 Next 
             </Button>
