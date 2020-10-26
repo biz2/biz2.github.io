986e80d3ecfbbbb06b75e7bbea309f9252027701
Vitor <bigellivitor@gmail.com>
2020-10-26 19:34:17 -0300

    Add custom fonts and home page phrase

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+import svg_finance_1 from '../../assets/finance_1.svg'
 
 const Home  = () => {
 
     return (
-        <Container fluid className='hero'/>
+        <Container fluid className='hero'>
+            <Row>
+                <Col xs={6} className='home-col'>
+                    <h1> The next generation on investment plataforms</h1>
+                    <h4> Transparent. Co-participative. Decentralized.</h4>
+                </Col>
+                <Col xs={6} className='home-image-container'>
+                    &nbsp;
+                </Col>
+            </Row>
+        </Container>
     )
    
 
