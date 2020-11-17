7ff70033d16b0f92b65be03dda5382890983cc7f
Vitor Bigelli <vitorbigelli@hotmail.com>
2020-11-17 14:34:19 -0300

    Adjusts on layout

--- src/pages/Home/components/WhyUs/index.js
+++ src/pages/Home/components/WhyUs/index.js
@@ -9,6 +9,7 @@ import transgender_image from '../../../../assets/transgender.svg'
 import environment_image from '../../../../assets/environment.svg'
 import favela_image from '../../../../assets/favela.svg'
 import power from '../../../../assets/community.svg'
+import { ReactSVG } from 'react-svg'
 
 const WhyUs = () => {
     return (
@@ -20,7 +21,7 @@ const WhyUs = () => {
                     </Row>
                     <Row>
                         <Col className='banner'> 
-                            <h3> For us, is essential that investors know where they are investing and which social causes they support </h3>
+                            <h3> For us, it is essential that investors know where they are investing and which social causes they support </h3>
                         </Col>
                     </Row>
                     <Row>
@@ -43,29 +44,30 @@ const WhyUs = () => {
                         </Col>
                     </Row>
                     <Row>
-                        <Col className='small-image-container'>
-                            <img src={afro_image} alt='Black enterpreneur' className='small-image' /> 
+                        <Col className='small-image-container remove-stroke'>
+                            <ReactSVG src={afro_image} alt='Black enterpreneur' className='small-image' /> 
                             <p>Black <br/> enterpreneurs</p>
                         </Col>
-                        <Col className='small-image-container'>
-                            <img src={woman_image} alt='Woman enterpreneur' className='small-image' />
+                        <Col className='small-image-container remove-stroke'>
+                            <ReactSVG src={woman_image} alt='Woman enterpreneur' className='small-image' />
                             <p>Woman <br/> enterpreneurs</p>
                         </Col>
                         <Col className='small-image-container'>
-                            <img src={transgender_image} alt='Transgender enterpreneur' className='small-image' />
+                            <ReactSVG src={transgender_image} alt='Transgender enterpreneur' className='small-image' />
                             <p>LGBT+ <br/> enterpreneurs</p>
                         </Col>
                     </Row>
                     <Row>
                         <Col className='small-image-container'>                          
-                            <img src={environment_image} alt='Environmental enterpreneur' className='small-image' />                              
+                            <ReactSVG src={environment_image} alt='Environmental enterpreneur' className='small-image' />                              
                             <p>Environmental <br/> causes</p>                          
                         </Col>
                         <Col className='small-image-container'>
-                            <img src={favela_image} alt='Favela enterpreneur' className='small-image' />                            
+                            <ReactSVG src={favela_image} alt='Favela enterpreneur' className='small-image' />                            
                             <p>Social <br/> causes</p>                          
-                        </Col>    
+                        </Col>   
                     </Row> 
+                    <Row> <Col> &nbsp; </Col></Row>
                     <Row> <Col> &nbsp; </Col></Row>
                     <Row> 
                         <Col> 
@@ -74,11 +76,11 @@ const WhyUs = () => {
                         </Col>
                     </Row>
                     <Row>
-                        <Col>
-                            <img src={power} alt='Power to people' className='image' />
+                        <Col className='d-flex flex-column justify-content-center align-items-center'>
+                            <h5 className='text-right p-1'> By using consensus, all investors will be responsible for “auditing” companies to make sure they are aligned with the principles they promise. </h5>
                         </Col>
                         <Col>
-                            <h5> By using consensus, all investors will be responsible for “auditing” companies to make sure they are aligned with the principles they promise. </h5>
+                            <img src={power} alt='Power to people' className='image' />
                         </Col>
                     </Row>
                 </Container>
