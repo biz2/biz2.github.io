670b0a07e099768dc8399ea4e7821f6e40de51d8
Vitor Bigelli <vitorbigelli@hotmail.com>
2020-11-22 13:02:21 -0300

    Add mobile menu

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -3,10 +3,12 @@ import React, { useState, useRef, useEffect } from 'react'
 import logo from '../../assets/logo.png'
 
 import { Nav, Button, Navbar, NavbarBrand } from 'react-bootstrap'
+import { IoIosRocket } from 'react-icons/io'
 
 const NavigationBar = (props) => {
 
     const [ navBackground, setNavBackground ] = useState(false)
+    const [ navIsOpen, toggleNav ] = useState(false)
 
     const navRef = useRef()
     
@@ -34,7 +36,7 @@ const NavigationBar = (props) => {
                     </Nav.Link >
             </NavbarBrand>
             <Nav
-              className='mr-auto'
+              className='mr-auto navbar-links'
             >
                 <Nav.Link href="#about" > About Us </Nav.Link>
                 &nbsp;
@@ -50,10 +52,35 @@ const NavigationBar = (props) => {
                 &nbsp;
                 <Nav.Link href="/register" > Projetos em captação    </Nav.Link> */}
             </Nav>
+            <Nav className='ml-auto'>
+              <Nav.Link href="#form" as={Button} variant='primary' className='text-white blue-bg' > 
+                <IoIosRocket /> 
+                &nbsp;
+                Be first! 
+              </Nav.Link> 
+              <Button onClick={() => toggleNav(!navIsOpen)} className={ navIsOpen ? 'nav-button show' : 'nav-button'} >
+                <div />
+                <div />
+                <div />
+              </Button>
+
+            </Nav>
             <Nav
-                className='ml-auto'
+              className={ navIsOpen ? 'navbar-mobile show' : 'navbar-mobile'}
             >
-              <Nav.Link href="#form" as={Button} variant='primary' className='text-white blue-bg' > Notify me of the launch </Nav.Link>
+                <Nav.Link href="#about" > About Us </Nav.Link>
+                &nbsp;
+                <Nav.Link href="#whyus" > Why Us </Nav.Link>
+                &nbsp;
+                <Nav.Link href="#howitworks" > How it Works </Nav.Link>
+                {/* &nbsp;
+                <Nav.Link href="/register" > Quero investir  </Nav.Link>
+                &nbsp;
+                <Nav.Link href="/register" > Quero captar  </Nav.Link> */}
+                &nbsp;
+                {/* <Nav.Link href="/register" > Projetos captados    </Nav.Link>
+                &nbsp;
+                <Nav.Link href="/register" > Projetos em captação    </Nav.Link> */}
             </Nav>
         </Navbar>
             
