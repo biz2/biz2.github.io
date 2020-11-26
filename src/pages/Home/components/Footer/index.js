ec8bebca4eab0faa580f84b3fa899aa199edcb27
Vitor Bigelli <vitorbigelli@hotmail.com>
2020-11-26 15:27:23 -0300

    Minimal adjustsments on footer and nav

--- src/pages/Home/components/Footer/index.js
+++ src/pages/Home/components/Footer/index.js
@@ -9,27 +9,29 @@ const Footer = () => {
             <Col className='footer'>
                 <Container>
                     <Row>
-                        <Col className='text-left'> 
+                        <Col className='d-flex flex-column text-left py-3' xs={12} md={3}> 
                             <img src={logo} className='nav-logo' alt='biz2 logo' />  
                         </Col> 
-                        <Col>
-                        <Nav className='flex-column text-left' > 
+                        <Col xs={12} md={3} className='py-3'>
+                        <Nav className='flex-column text-left'> 
                             <h5> ABOUT </h5>
                             <Nav.Link href="#about" > About Us </Nav.Link>
                             <Nav.Link href="#howitworks" > How it Works </Nav.Link>
                             <Nav.Link href="#whyus" > Why Us </Nav.Link>
                         </Nav> 
                         </Col> 
-                        <Col>
-                        <Nav className='flex-column text-left' >
+                        <Col xs={12} md={3} className='py-3'>
+                        <Nav className='flex-column text-left'>
                             <h5> LEGAL </h5>
                             <Nav.Link href="#about" > Crowdfunding Regulation </Nav.Link>
                             <Nav.Link href="#howitworks" > Terms of Use </Nav.Link>
                         </Nav> 
                         </Col>
-                        <Col className='text-left'>
-                            <FaFacebook size={30} className='m-1' color='#1ea4db' />
-                            <FaLinkedin size={30} className='m-1' color='#1ea4db' />
+                        <Col className='text-left py-3' xs={12} md={3}>
+                            <div>
+                                <FaFacebook size={30} className='my-0 mx-1' color='#1ea4db' />
+                                <FaLinkedin size={30} className='my-0 mx-1' color='#1ea4db' />
+                            </div>
                             <p className='m-1'> Rua Teste, 123, Centro, São Paulo, SP </p>
                         </Col>
                     </Row>
