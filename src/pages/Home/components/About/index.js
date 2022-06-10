fe59e75493a59010aab99421232b524d78d18a80
rodrigobiz2 <94414377+rodrigobiz2@users.noreply.github.com>
2022-06-10 10:52:36 -0300

    Update index.js

--- src/pages/Home/components/About/index.js
+++ src/pages/Home/components/About/index.js
@@ -17,7 +17,8 @@ const About = () => {
                     <Row>
                         <Col xs={12} className='banner'>
                             <div> 
-                                <p> Empowering startup shareholders and profitable SME owners, unlocking value created in private equity at anytime </p>
+                                //<p> Empowering startup shareholders and profitable SME owners, unlocking value created in private equity at anytime </p>
+                                <p> Empoderamos acionistas de pequenas e médias empresas e startups, destravando valor em capital privado em qualquer momento </p>
                             </div>
                         </Col>
                     </Row>
@@ -27,10 +28,15 @@ const About = () => {
                             <img src={about_svg} alt='Desenho de um homem segurando um cifrão. Há gráficos de barras no fundo.' className='image' />
                         </Col>
                         <Col xs={12} md={6} className='paragraph-container'>
-                            <p> 
+                            /*<p> 
                                 The financial market will face even more changes in the future, and we offer to individual 
                                 investors the opportunity to invest in new companies and, until now inaccessible, private equity funds with
                                 relevant profitability history, ensuring reliability using cutting-edge technologies.
+                            </p>*/
+                            <p> 
+                                As mudanças no mercado financeiro estão só começando e nós oferecemos a oportunidade de investidores individuais
+                                investirem em novas empresas e fundos antes inacessíveis, exatamente em seu momento de inflexão,
+                                onde possuem histórico relevante de rentabilidade, assegurando confiança e tecnologia de ponta em todo o processo.
                             </p>
                         </Col>
                     </Row>
